add73a9ee82c5c6d58d275bc9c26e39c4b44afcc
Morgan Everec <migran1995@gmail.com>
2021-07-18 21:08:48 +0300

    first
    use an alternative control to represent a list of string data

---
+++ wrapmodeList/wrapmodeList/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace wrapmodeList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addMethod = new System.Windows.Forms.Button();
+            this.removeMethod = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(269, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(200, 218);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // addMethod
+            // 
+            this.addMethod.Location = new System.Drawing.Point(269, 252);
+            this.addMethod.Name = "addMethod";
+            this.addMethod.Size = new System.Drawing.Size(75, 23);
+            this.addMethod.TabIndex = 3;
+            this.addMethod.Text = "Добавить";
+            this.addMethod.UseVisualStyleBackColor = true;
+            this.addMethod.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // removeMethod
+            // 
+            this.removeMethod.Location = new System.Drawing.Point(394, 252);
+            this.removeMethod.Name = "removeMethod";
+            this.removeMethod.Size = new System.Drawing.Size(75, 23);
+            this.removeMethod.TabIndex = 4;
+            this.removeMethod.Text = "Удалить";
+            this.removeMethod.UseVisualStyleBackColor = true;
+            this.removeMethod.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removeMethod);
+            this.Controls.Add(this.addMethod);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addMethod;
+        private System.Windows.Forms.Button removeMethod;
+    }
+}
+
---
+++ wrapmodeList/wrapmodeList/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wrapmodeList
+{
+    public partial class Form1 : Form
+    {
+       //TODO добавить два столба в dataTable из прошлого моего проетка, который я скинул (фильтр методов и контекста)
+       //настроить стиль отображения, чтобы нельзя было отличить датагрид от листбокса (опционально, но думаю справишься)
+       //если будут строки со словами, длина которых превышает ширину датагрида пытаться обработать их другим методом или просто довольствоваться еще одним скролбаром снизу(на твое усмотрение)
+
+        DataSet DS;
+        DataTable DT;
+        int counter;
+        public Form1()
+        {
+            InitializeComponent();
+            DS = new DataSet();
+            DT = new DataTable("currentMethods");
+            counter = 2;
+            //создание dataTable, в нашем случае добавишь уже в существующий новый столбец, а вернее два, которые будут отвечать за фильтры
+            DataColumn DC = new DataColumn("Method", typeof(string));
+            DT.Columns.Add(DC);
+            DS.Tables.Add(DT);
+            DataRow dr = DT.NewRow();
+            dr["Method"] = "method1";
+            DT.Rows.Add(dr);
+
+            dataGridView1.DataSource = DT; //привязка данных
+
+            //смотри свойства датагрида, я убрал хедеры строк и столбоц и сделал перенос по словам
+
+        }
+        
+        internal class CurrentMethod
+        {
+            public string method { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //добавить
+        {
+            DataRow dr = DT.NewRow();
+            dr["Method"] = "yet another some long-long method name that we have to use"+counter;
+            DT.Rows.Add(dr);
+            counter++;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //удалить
+        {
+            if (dataGridView1.SelectedCells.Count != 0)
+            {
+                int index = dataGridView1.SelectedCells[0].RowIndex;
+                DT.Rows.RemoveAt(index);
+            }
+            
+        }
+    }
+  
+        
+}
